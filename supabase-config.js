980a59d77c7b396250f3be7b5094a41165792d2d
Vivin <Vivin@Vivins-Mac.local>
2026-07-22 00:51:50 +0530

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.PLANWELL_SUPABASE = {
-  url: "PASTE_SUPABASE_PROJECT_URL_HERE",
-  anonKey: "PASTE_SUPABASE_ANON_KEY_HERE"
+  url: "https://vqccetkfpvejfbfxvayr.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxY2NldGtmcHZlamZiZnh2YXlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NDk5NTEsImV4cCI6MjEwMDIyNTk1MX0.WXb7DjdPTpF_KoZzN93LewGsH_QWvVz6eeR-15AoSow"
 };
